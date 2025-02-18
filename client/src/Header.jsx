299f2ef4893071810e4ed37fba45234367f48738
unknown <zaidsmadanat@gmail.com>
2025-02-18 12:49:57 -0800

    fixed styling

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,8 +4,10 @@ import LinkedLogo from "./assets/LinkedLogo.png"
 
 function Header() {
   return (
-    <header className="bg-light p-3 d-flex align-items-center justify-content-between">
-      <h1 className="fs-2 m-0">Days</h1>
+    <header
+      className="bg-light w-100 p-3 d-flex align-items-center justify-content-between"
+      style={{width: "100%"}}
+    >      <h1 className="mb-0">Days</h1>
       <div>
         <a 
           href="https://github.com/ZaidMadanat" 
