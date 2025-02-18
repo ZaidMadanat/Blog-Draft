ab7451c60b1cc564c011b07baf8e4d0e095adee7
unknown <zaidsmadanat@gmail.com>
2025-02-18 13:01:58 -0800

    finished draft blog, to change later

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,9 +5,9 @@ import LinkedLogo from "./assets/LinkedLogo.png"
 function Header() {
   return (
     <header
-      className="bg-light w-100 p-3 d-flex align-items-center justify-content-between"
-      style={{width: "100%"}}
-    >      <h1 className="mb-0">Days</h1>
+      className="w-100 p-3 d-flex align-items-center justify-content-between"
+      style={{width: "100%", backgroundColor: "#e3f2fd"}}
+    >      <h1 className="mb-0">The Days</h1>
       <div>
         <a 
           href="https://github.com/ZaidMadanat" 
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,17 +6,30 @@ function Home() {
 
   useEffect(() => {
     fetch("http://localhost:5000/api/posts")
-      .then(res => res.json())
-      .then(data => setPosts(data))
-      .catch(err => console.error("Error fetching posts:", err));
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error("Error fetching posts:", err));
   }, []);
 
   return (
-    <div className="container mt-4">
-      <ul className="list-group">
-        {posts.map(post => (
-          <li key={post.slug} className="list-group-item">
-            <Link to={`/${post.slug}`}>{post.title}</Link>
+    <div className="container py-4">
+      <ul 
+        className="list-group mx-auto" 
+        style={{ maxWidth: "600px" }}
+      >
+        {posts.map((post) => (
+          <li
+            key={post.slug}
+            className="list-group-item mb-3 border rounded"
+            style={{ backgroundColor: "#f9f9f9" }}
+          >
+            <Link
+              to={`/${post.slug}`}
+              className="text-decoration-none text-center d-block"
+              style={{ fontSize: "1.25rem", color: "#333" }}
+            >
+              {post.title}
+            </Link>
           </li>
         ))}
       </ul>
